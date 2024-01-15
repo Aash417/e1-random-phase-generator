fc915a22a5a9c85884b95ec5b20a2e20fbac621e
aashish_k <aashishkathat00@gmail.com>
2024-01-16 01:16:46 +0530

    build 1

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { faker } from '@faker-js/faker';
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer" hidden='true'>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a >
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        
+        <p>
+        {count && <RandomPhase/>}
+        </p>
+      </div>
+    </>
+  )
+}
+
+function RandomPhase() {
+  return faker.hacker.phrase();
+  
+}
+
+export default App
